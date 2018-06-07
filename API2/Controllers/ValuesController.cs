4a35cdac811da3d1ca964d8f171e7e1e600e4884
Peter F. Guenther <peter.guenther@gmail.com>
2018-06-07 13:46:55 -0400

    first commit

---
+++ API2/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using API2.Models;
+using System.Web.Http.Results;
+
+namespace API2.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        static int hits = 0;
+        // GET api/values/5
+        public int GetCounter()
+        {
+            hits++;
+            return hits;
+        }
+
+        public Card GetCard()
+        {
+            Card c = new Card();
+            c.Suit = "Hearts";
+            c.Value = "Queen";
+
+            return c;
+        }
+
+        public List<Product> GetCatalog()
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            List<Product> products = db.Products.ToList();
+
+            return products;
+        }
+
+        public Product GetProduct(int id)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            Product prod = (from p in db.Products
+                            where p.ProductID == id
+                            select p).Single();
+
+            return prod;
+        }
+
+        public List<Product> GetProductByName(string id)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            List<Product> prod = (from p in db.Products
+                            where p.ProductName.Contains(id)
+                            select p).ToList();
+
+            return prod;
+        }
+
+    }
+}
---
+++ API2/Models/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API2.Models
+{
+    public class Card
+    {
+        public string Suit { get; set; }
+        public string Value { get; set; }
+    }
+}
